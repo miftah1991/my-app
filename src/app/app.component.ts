9830734b05d1d69c793bf6151ac621b7e7a72024
Miftah Amin <miftah.amin1991@gmail.com>
2022-09-03 17:44:07 +0430

    New Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,96 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  Students:any[];
+  countrydetail:any[];
+  
+  constructor(){
+    this.countrydetail=[
+      {
+        country:'Af',
+        peoples:[{ name:'ahmad'},{ name:'af2'},{ name:'af3'},{ name:'af4'},]
+      },
+      {
+        country:'PK',
+        peoples:[{ name:'ALI'},{ name:'KHAN'},{ name:'PK2'},{ name:'PK3'},]
+      },
+    ]
+    this.Students=[
+      {
+        studentid:1,
+        name:'Miftah',
+        gender:'Male',
+        Age:32,
+        Course:'MIS'
+
+     },
+      {
+        studentid:2,
+        name:'Qasim',
+        gender:'Male',
+        Age:25,
+        Course:'IT'
+     },
+      {
+        studentid:3,
+        name:'Ibrahim',
+        gender:'Male',
+        Age:23,
+        Course:'MS'
+     },
+      {
+        studentid:4,
+        name:'Ali',
+        gender:'Male',
+        Age:34,
+        Course:'bcs'
+     },
+    ]
+  }
+  getmorestudents():void{
+    this.Students=[
+      {
+        studentid:1,
+        name:'Miftah',
+        gender:'Male',
+        Age:32,
+        Course:'MIS'
+
+     },
+      {
+        studentid:2,
+        name:'Qasim',
+        gender:'Male',
+        Age:25,
+        Course:'IT'
+     },
+      {
+        studentid:3,
+        name:'Ibrahim',
+        gender:'Male',
+        Age:23,
+        Course:'MS'
+     },
+      {
+        studentid:4,
+        name:'Ali',
+        gender:'Male',
+        Age:34,
+        Course:'bcs'
+     },
+     {
+      studentid:5,
+      name:'new emp',
+      gender:'Female',
+      Age:16,
+      Course:'IT'
+   },
+    ]
+  }
+  trackbystudentid(index:number,student:any):string{
+    return student.studentid;
+  }
+  
+
+
 }
