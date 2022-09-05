ab107985aa8e86a1ff0e72bf228bfe599a829210
Miftah Amin <miftah.amin1991@gmail.com>
2022-09-05 07:45:51 +0430

    add redirect route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FundamentalsComponent } from './fundamentals/fundamentals.component';
+import { MaincomponentComponent } from './maincomponent/maincomponent.component';
 import { StudentComponent } from './student/student.component';
 const routes: Routes = [
+  {path :'', redirectTo:'main',pathMatch:'full'},
+  {path :'main',component:MaincomponentComponent},
   {path :'student',component:StudentComponent},
   {path :'fundamentals',component:FundamentalsComponent},
 
